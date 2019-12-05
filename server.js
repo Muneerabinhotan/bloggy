c4c6461699bbf3d82a856846a810042434eac808
Muneera Bin Hotan <muneerabinhotan@gmail.com>
2019-12-05 11:36:42 +0300

    Initial Commit

---
+++ server.js
@@ -0,0 +1,14 @@
+// Require Necessary NPM Packages
+const express = require('express');
+const mongoose = require('mongoose');
+
+// Instantiate Express Application Object || Make an Express App
+const app = express();
+
+// Define a PORT for the API to run on 
+const port = process.env.PORT || 5000; // if the env has a number use it if not use 5000 || proce... comes from node
+
+// Start the server to listen for requests on a given port
+app.listen(port, () =>{
+    console.log(`blogy is listening on port ${port}`);
+});
